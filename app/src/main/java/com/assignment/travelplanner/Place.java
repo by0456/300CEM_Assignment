d4ef7b5235ebe4f7cb513ca9e9c404a08e24e399
by0456 <58273338+by0456@users.noreply.github.com>
2020-05-08 22:04:16 +0800

    first time commit

--- app/src/main/java/com/assignment/travelplanner/Place.java
+++ app/src/main/java/com/assignment/travelplanner/Place.java
@@ -1,4 +1,41 @@
 package com.assignment.travelplanner;
 
 public class Place {
+
+    private String name;
+    private String description;
+    private String latitude;
+    private String longitude;
+
+    public Place(String name, String description, String latitude, String longitude){
+        this.name = name;
+        this.description = description;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public String getLatitude(){
+        return this.latitude;
+    }
+
+    public String getLongitude(){
+        return this.longitude;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setDescription(String description){
+        this.description = name;
+    }
+
 }
